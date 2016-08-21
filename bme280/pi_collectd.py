21367a76d5e3fa3c3cad6c7692041e38f6d77bbc
Anthony Virtuoso <avirtuos@gmail.com>
2016-08-21 02:09:15 +0000

    Adding single instance support, a real logger, and refined RRD settings

--- bme280/pi_collectd.py
+++ bme280/pi_collectd.py
@@ -8,6 +8,13 @@ import urllib2
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from bme280 import Bme280Sensor
+from tendo import singleton
+import logging
+import logging.handlers
+
+#Ensures only one instance of this script is running at a time
+me = singleton.SingleInstance()	
+
 
 nodeName = "pi-139 bme"
 rrdPath = "/home/pi/data/"
@@ -27,21 +34,32 @@ data_sources = ['DS:temperature:GAUGE:600:U:U',
                 'DS:humidity:GAUGE:600:U:U',
                 'DS:pressure:GAUGE:600:U:U' ]
 
+
+LOG_FILENAME = '/home/pi/data/pi_collectd.log'
+
+# Set up a specific logger with our desired output level
+logger = logging.getLogger('PiCollectd')
+logger.setLevel(logging.DEBUG)
+
+# Add the log message handler to the logger
+handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1000000, backupCount=2)
+logger.addHandler(handler)
+
 def getOrCreateRrd():
 	if(not os.path.isfile(rrdFile)):
-		print "Creating " + rrdFile
+		logger.info("Creating " + rrdFile)
  		rrdtool.create( rrdFile,
                 '--start', '-10',
-                '--step', '1',
+                '--step', '10',
                 data_sources,
                 'RRA:AVERAGE:0.5:1:320000',
-                'RRA:MIN:0.5:3600:20000',
-                'RRA:MAX:0.5:3600:20000',
-                'RRA:AVERAGE:0.5:3600:20000')
+                'RRA:MIN:0.5:360:40000',
+                'RRA:MAX:0.5:360:40000',
+                'RRA:AVERAGE:0.5:360:40000')
 
 def updateGraph(lastUpdated, awsS3Bucket, awsS3Key, graphPath):
-	if(time.time() - lastUpdated > 60):
-		print "Updating Graph..."
+	if(time.time() - lastUpdated > 300):
+		logger.info("Updating Graph...")
 		rrdtool.graph(graphPath,
 	            '--imgformat', 'PNG',
 	            '--width', '500',
@@ -52,7 +70,7 @@ def updateGraph(lastUpdated, awsS3Bucket, awsS3Key, graphPath):
 	            '--title', 'pi-0-139 BME280 - Temperature, Humidity, Pressure',
 				'--watermark', 'Generated at ' + strftime("%m-%d-%Y %H:%M:%S", time.localtime()),
 	            "DEF:temperature_raw=/home/pi/data/bme280.rrd:temperature:AVERAGE",
-				"DEF:temperature_avg=/home/pi/data/bme280.rrd:temperature:AVERAGE:step=3600",
+				"DEF:temperature_avg=/home/pi/data/bme280.rrd:temperature:AVERAGE:step=360",
 				"DEF:temperature_min=/home/pi/data/bme280.rrd:temperature:MIN",
 				"DEF:temperature_max=/home/pi/data/bme280.rrd:temperature:MAX",
 				"DEF:pressure=/home/pi/data/bme280.rrd:pressure:AVERAGE",
@@ -64,10 +82,10 @@ def updateGraph(lastUpdated, awsS3Bucket, awsS3Key, graphPath):
 				"GPRINT:humidity:MAX:Max\:%5.2lf %s",
 				"GPRINT:humidity:MIN:Min\:%5.2lf %s\\n",
 				"LINE2:scaled_pressure#0000FF:Pressure hpa   ",
-				"GPRINT:pressure:LAST:Last\:%5.2lf %s",
-				"GPRINT:pressure:AVERAGE:Avg\:%5.2lf %s",
-				"GPRINT:pressure:MAX:Max\:%5.2lf %s",
-				"GPRINT:pressure:MIN:Min\:%5.2lf %s\\n",
+				"GPRINT:pressure:LAST:Last\:%5.3lf %s",
+				"GPRINT:pressure:AVERAGE:Avg\:%5.3lf %s",
+				"GPRINT:pressure:MAX:Max\:%5.3lf %s",
+				"GPRINT:pressure:MIN:Min\:%5.3lf %s\\n",
 				"LINE1:temperature_raw#FF0000:Temperature    ",
 				"GPRINT:temperature_raw:LAST:Last\:%5.2lf %s",
 				"GPRINT:temperature_raw:AVERAGE:Avg\:%5.2lf %s",
@@ -95,7 +113,7 @@ def updateGraph(lastUpdated, awsS3Bucket, awsS3Key, graphPath):
 
 def s3Upload(targetBucket, targetKey, fromPath ):
 	global awsAccessKey, awsSecretKey
-	print "Uploading " + fromPath + " to " + "s3:" + targetBucket + "/" + targetKey + " awsAccessKey: " + awsAccessKey + " awsSecretKey: " + awsSecretKey
+	logger.info("Uploading " + fromPath + " to " + "s3:" + targetBucket + "/" + targetKey)
 	conn = S3Connection(awsAccessKey,awsSecretKey)
 	bucket = conn.get_bucket(targetBucket)
 	k = Key(bucket)
@@ -104,26 +122,37 @@ def s3Upload(targetBucket, targetKey, fromPath ):
 
 
 def main():
+	sensor = Bme280Sensor()
+
+	warmUp = 0
+	while warmUp < 15:
+		logger.info("Warm Up: " + str(warmUp))
+		warmUp = warmUp + 1
+		(chip_id, chip_version) = sensor.readBME280ID()
+		logger.info("Chip ID     :" + str(chip_id))
+		logger.info("Version     :" + str(chip_version))
+		temperature,pressure,humidity = sensor.readBME280All()
+		temperature = (temperature * 9/5) + 32
+		logger.info("Temperature : " + str(temperature) + "C")
+		logger.info("Pressure : " + str(pressure) + "hPa")
+		logger.info("Humidity : " + str(humidity) + "%")
+		time.sleep(1)
+
 	lastUpdated = 0
 	while(1):
 		getOrCreateRrd()
-
-		sensor = Bme280Sensor()
-		(chip_id, chip_version) = sensor.readBME280ID()
-		print "Chip ID     :", chip_id
-		print "Version     :", chip_version
 		temperature,pressure,humidity = sensor.readBME280All()
 		temperature = (temperature * 9/5) + 32
-		print "Temperature : ", temperature, "C"
-		print "Pressure : ", pressure, "hPa"
-		print "Humidity : ", humidity, "%"
+		logger.info("Temperature : " + str(temperature) + "C")
+		logger.info("Pressure : " + str(pressure) + "hPa")
+		logger.info("Humidity : " + str(humidity) + "%")
 
 		ret = rrdtool.update(rrdFile, '%s:%s:%s:%s' %(time.time(),temperature, humidity, pressure));
-		print "Updated RRD ", ret
+		logger.info("Updated RRD " + ret)
 
 		lastUpdated = updateGraph(lastUpdated, awsS3Bucket, awsS3Key, graphPath)
 
-		time.sleep(1)
+		time.sleep(10)
 
 if __name__=="__main__":
    main()
